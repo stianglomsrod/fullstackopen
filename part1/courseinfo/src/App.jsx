5127563c0be778c10498c8fd4d23d0033fc1eb4b
stianglomsrod <112327355+stianglomsrod@users.noreply.github.com>
2026-07-11 15:53:15 +0200

    Refactor App component to pass course object instead of separate parts

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,7 +1,8 @@
 const Header = (props) => {
+  console.log(props.course.parts[0].name);
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </>
   );
 };
@@ -17,44 +18,45 @@ const Part = (props) => {
 };
 
 const Content = (props) => {
+  console.log(props.course.parts[0].name);
   return (
     <>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+      <Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises} />
+      <Part part={props.course.parts[1].name} exercises={props.course.parts[1].exercises} />
+      <Part part={props.course.parts[2].name} exercises={props.course.parts[2].exercises} />
     </>
   );
 };
 
 const Total = (props) => {
-  console.log(props.parts[0].name);
+  console.log(props.course.parts[0].name);
 
   return (
     <>
       <p>
         Number of exercises{" "}
-        {props.parts[0].exercises +
-          props.parts[1].exercises +
-          props.parts[2].exercises}
+        {props.course.parts[0].exercises +
+          props.course.parts[1].exercises +
+          props.course.parts[2].exercises}
       </p>
     </>
   );
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-
-  const parts = [
-    { name: "Fundamentals of React", exercises: 10 },
-    { name: "Using props to pass data", exercises: 7 },
-    { name: "State of a component", exercises: 14 },
-  ];
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: "Fundamentals of React", exercises: 10 },
+      { name: "Using props to pass data", exercises: 7 },
+      { name: "State of a component", exercises: 14 },
+    ],
+  };
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   );
 };
