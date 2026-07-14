9ea69bed1fdbeba5977a207bd83d2b9bb80b7b52
stianglomsrod <112327355+stianglomsrod@users.noreply.github.com>
2026-07-14 16:50:07 +0200

    did 2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+
+
+const Name = ({ name }) => {
+
+  return (
+    <>
+      <li>{name}</li>
+    </>
+  )
+} 
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNameChange = (e) => {
+    console.log(e.target.value);
+    setNewName(e.target.value);
+  };
+
+  const addName = (e) => {
+    console.log(e.target);
+    e.preventDefault();
+    const nameObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(nameObject));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      
+        {persons.map((person) => (
+          <Name key={person.name} name={person.name} />
+        ))}
+      
+    </div>
+  );
+};
+
+export default App;
