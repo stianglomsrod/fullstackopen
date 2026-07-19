e90e8f9cba3a44ac33d8702af3c83eadc03c88a5
stianglomsrod <112327355+stianglomsrod@users.noreply.github.com>
2026-07-19 16:23:25 +0200

    did 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios';
 
 const Filter = ({ value, onChange }) => {
   return (
@@ -41,12 +42,7 @@ const PersonForm = (props) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
@@ -54,8 +50,18 @@ const App = () => {
     person.name.toLowerCase().includes(filter.toLowerCase()),
   );
 
+
+  useEffect(() => {
+    axios.
+    get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  }, [])
+
+
+
   const handleNameChange = (e) => {
-    console.log(e.target.value);
     setNewName(e.target.value);
   };
 
