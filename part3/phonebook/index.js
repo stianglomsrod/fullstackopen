8a4cb418b4fefad90ccc945567a6174eec57d7a2
stianglomsrod <112327355+stianglomsrod@users.noreply.github.com>
2026-07-23 14:28:03 +0200

    did 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,12 @@ const app = express();
 const morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (req) => {
+    return req.method === 'POST' ? JSON.stringify(req.body) : ''
+})
+
+app.use(morgan(':method :url :status: :res[content-length] - :response-time ms :body'))
 
 let persons = [
   {
