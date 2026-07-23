f87c933bf6d7cfad6e4e96ea03aec4cbe708d0b2
stianglomsrod <112327355+stianglomsrod@users.noreply.github.com>
2026-07-23 12:55:15 +0200

    did 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -36,18 +36,22 @@ app.get("/info", (req, res) => {
   );
 });
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    const person = persons.find(person => person.id === id)
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((person) => person.id === id);
 
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
-})
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
 
-
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
