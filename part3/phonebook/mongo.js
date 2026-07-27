3bd4f17794c5461d0f92a33b3ef247289ec929a8
stianglomsrod <112327355+stianglomsrod@users.noreply.github.com>
2026-07-27 12:47:16 +0200

    did 3.12

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://stianglomsrod12345:${password}@cluster0.2feukfv.mongodb.net/?appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url, { family: 4 });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 3) {
+  Person.find({}).then((persons) => {
+    persons.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+} else {
+  const person = new Person({
+    name: name,
+    number: number,
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
